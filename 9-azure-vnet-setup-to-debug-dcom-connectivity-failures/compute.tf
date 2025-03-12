97e6a78c908e652edbba494807c80c60ecd3b14c
Jay Long <jaylong255@gmail.com>
2025-03-12 12:46:59 -0500

    launching a vm to represent the app server

--- 9-azure-vnet-setup-to-debug-dcom-connectivity-failures/compute.tf
+++ 9-azure-vnet-setup-to-debug-dcom-connectivity-failures/compute.tf
@@ -21,3 +21,26 @@ resource "azurerm_windows_virtual_machine" "nvr_vm" {
     version   = "latest"
   }
 }
+
+# Windows VM for Application Server
+resource "azurerm_windows_virtual_machine" "app_vm" {
+  name                  = "app-vm"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_D2s_v3"
+  admin_username        = "adminuser"
+  admin_password        = "P@ssw0rd1234!" # Replace with a secure password
+  network_interface_ids = [azurerm_network_interface.app_nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
--- 9-azure-vnet-setup-to-debug-dcom-connectivity-failures/outputs.tf
+++ 9-azure-vnet-setup-to-debug-dcom-connectivity-failures/outputs.tf
@@ -1,3 +1,7 @@
 output "nvr_public_ip" {
   value = azurerm_public_ip.nvr_public_ip.ip_address
 }
+
+output "app_public_ip" {
+  value = azurerm_public_ip.app_public_ip.ip_address
+}
