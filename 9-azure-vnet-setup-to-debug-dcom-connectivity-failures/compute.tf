829040532a0f2af22ca8f5ef605e2f466fbcac29
Jay Long <jaylong255@gmail.com>
2025-03-12 12:45:38 -0500

    launching a vm to emulate the nvr machine

---
+++ 9-azure-vnet-setup-to-debug-dcom-connectivity-failures/compute.tf
@@ -0,0 +1,23 @@
+
+# Windows VM for NVR
+resource "azurerm_windows_virtual_machine" "nvr_vm" {
+  name                  = "nvr-vm"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_D2s_v3"
+  admin_username        = "adminuser"
+  admin_password        = "P@ssw0rd1234!" # Replace with a secure password
+  network_interface_ids = [azurerm_network_interface.nvr_nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
