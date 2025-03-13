178cdc103eddea2cfee7ce820508afc73c11ce80
Jay <jaylong255@gmail.com>
2025-03-13 20:43:34 +0000

    moving outputs for simplicity

--- 9-azure-vnet-setup-to-debug-dcom-connectivity-failures/compute.tf
+++ 9-azure-vnet-setup-to-debug-dcom-connectivity-failures/compute.tf
@@ -44,3 +44,11 @@ resource "azurerm_windows_virtual_machine" "app_vm" {
     version   = "latest"
   }
 }
+
+output "nvr_public_ip" {
+  value = azurerm_public_ip.nvr_public_ip.ip_address
+}
+
+output "app_public_ip" {
+  value = azurerm_public_ip.app_public_ip.ip_address
+}
